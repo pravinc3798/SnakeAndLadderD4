6f95eafeb3f0e306a0e3c182a220911bcfaf263a
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-10 17:53:59 +0530

    Initial Push from local

---
+++ SinglePlayer.cs
@@ -0,0 +1,20 @@
+﻿namespace SnakeAndLadder
+{
+    public class SinglePlayer
+    {
+        public static void Play()
+        {
+            Console.Write("Enter Player Name : ");
+            var player = new Player(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            byte end = 0;
+            while (end < 100)
+            {
+                Console.WriteLine("{0}'s Turn >>>> Press Enter to Roll Die", player.Name);
+                Console.ReadLine();
+                end = player.Move();
+            }
+        }
+    }
+}
