97be02aaf399de110aa67286c2cde83796dee144
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-10 17:53:49 +0530

    Initial Push from local

---
+++ MultiPlayer.cs
@@ -0,0 +1,35 @@
+﻿namespace SnakeAndLadder
+{
+    public class MultiPlayer
+    {
+        public static void Play()
+        {
+            Console.Write("Enter Player 1 Name : ");
+            var player1 = new Player(Console.ReadLine());
+
+            Console.Write("Enter Player 2 Name : ");
+            var player2 = new Player(Console.ReadLine());
+
+            Console.WriteLine("\n");
+
+            byte end1 = 0;
+            byte end2 = 0;
+
+            while (end1 < 100 && end2 < 100)
+            {
+                Console.WriteLine("{0}'s Turn >>>> Press Enter to Roll Die ", player1.Name);
+                Console.ReadLine();
+                end1 = player1.Move();
+
+                Console.WriteLine("{0}'s Turn >>>> Press Enter to Roll Die ", player2.Name);
+                Console.ReadLine();
+                end2 = player2.Move();
+
+                if (end1 == 100)
+                    Console.WriteLine("\n\n >>>>>{0} - WON !!!!  ", player1.Name);
+                else if (end2 == 100)
+                    Console.WriteLine("\n\n >>>>>{0} - WON !!!!  ", player2.Name);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SnakeAndLadder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n ------ SNAKE AND LADDER ------ \n 1. Single Player \n 2. Two Player \n ");
+            Console.Write(" Input number of Players (1 or 2) : ");
+            var input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1": SinglePlayer.Play(); break;
+                case "2": MultiPlayer.Play(); break;
+                default: Console.WriteLine("Invalid Input!!"); break;
+            }
+        }
+    }
+}
